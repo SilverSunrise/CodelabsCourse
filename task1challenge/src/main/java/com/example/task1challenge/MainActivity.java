f353886ed76028de7c59b873a2b73d1c30652f9a
SilverSunrise <lakistraik6@gmail.com>
2019-12-19 22:39:35 +0200

    Task1-Challenge

--- task1challenge/src/main/java/com/example/task1challenge/MainActivity.java
+++ task1challenge/src/main/java/com/example/task1challenge/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android;
+package com.example.task1challenge;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,25 +10,25 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
-    private TextView mShowCount;
+    private TextView ShowCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+        ShowCount = (TextView) findViewById(R.id.ShowCount);
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this,R.string.toast_message, Toast.LENGTH_SHORT);
+    public void btnToast(View view) {
+        Toast toast = Toast.makeText(this,"HelloToast",Toast.LENGTH_SHORT);
         toast.show();
-        
     }
 
-    public void CountUp(View view) {
+    public void btnCount(View view) {
         mCount++;
-        if (mShowCount!= null){
-           mShowCount.setText(Integer.toString(mCount));
+        if(ShowCount != null){
+            ShowCount.setText(Integer.toString(mCount));
         }
     }
 }
